a8cac0e7d7d8b38d54ac8b3fa3c083384517151e
nulltitleexception <clbprfftt@gmail.com>
2014-10-16 14:58:44 -0400

    stuff
    things

--- main.cpp
+++ main.cpp
@@ -11,10 +11,30 @@ int main(int argc, char** args){
 		std::cout << "SDL initialization failed. SDL_Error: " << SDL_GetError() << std::endl;
 		return 1;
 	}
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+
 	SDL_Window* window = SDL_CreateWindow("Space Game", 30, 30, 800, 600, SDL_WINDOW_OPENGL);
 
+	SDL_GLContext context = SDL_GL_CreateContext(window);
+	SDL_GL_MakeCurrent(window, context);
+
+	glewExperimental = GL_TRUE;
+	GLenum err = glewInit();
+	if (err != GLEW_OK)
+	{
+		//error
+	}
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+
 	SDL_Event event;
-	while (true){
+	bool shouldExit = false;
+	while (!shouldExit){
 		while (SDL_PollEvent(&event)){
 			switch (event.type){
 			case SDL_KEYDOWN:
@@ -23,11 +43,27 @@ int main(int argc, char** args){
 					std::cout << "a was pressed" << std::endl;
 					break;
 				}
-
+			case SDL_QUIT:
+				shouldExit = true;
+				break;
+			case SDL_WINDOWEVENT:
+				switch (event.window.event){
+				case SDL_WINDOWEVENT_CLOSE:
+					shouldExit = true;
+					break;
+				}
+				break;
 			}
 		}
 		//other stuff
 	}
+
+
+	//draw
+	//SDL_GL_MakeCurrent(window, context);
+	//SDL_GL_SwapWindow(window);
+	SDL_GL_DeleteContext(context);
+	SDL_DestroyWindow(window);
 
 	int retval;
 	std::cin >> retval;
